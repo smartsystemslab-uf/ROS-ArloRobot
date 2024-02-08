dff6f674666840ee4a2469ce62ffc9af4ff75bd1
tjcanro <tjcanro@gmail.com>
2024-02-08 12:58:49 -0500

    fixed bug

--- XY-160D-controller/scripts/motor-controller-interface.py
+++ XY-160D-controller/scripts/motor-controller-interface.py
@@ -1,19 +1,24 @@
 import Jetson.GPIO as GPIO
 
+
 def set_PWM_pin(PIN_nmb):
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(PIN_nmb, GPIO.OUT)
     xy_pwm = GPIO.PWM(PIN_nmb, 100)  # PWM object creation on pin 33, with 100Hz frequency
     xy_pwm.start(0)
+    return xy_pwm
 
 
 # Set duty cycle changes the duty cycle of a specific pwm_object
 def set_duty_cycle(duty_cycle, pwm_obj):
     pwm_obj.ChangeDutyCycle(duty_cycle)
+
+
 def main():
     # General GPIO setup
-    set_PWM_pin(33)
+    xy_pwm = set_PWM_pin(33)
     set_duty_cycle(70, xy_pwm)
+
 
 if __name__ == "__main__":
     main()
